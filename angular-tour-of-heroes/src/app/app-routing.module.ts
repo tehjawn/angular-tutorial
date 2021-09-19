78b73a0b793e02a0d2115a4b7ad1bce3283bb61c
tehjawn <tehjawn@gmail.com>
2021-09-19 18:11:14 -0400

    Tutorial: Top Hero Dashboard + Default Root Route

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo:'/dashboard',
+    pathMatch: 'full',
+  },
+  {
     path: 'heroes',
-    component: HeroesComponent
+    component: HeroesComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
   }
 ]
 
