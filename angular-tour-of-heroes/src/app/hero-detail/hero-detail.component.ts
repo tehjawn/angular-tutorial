432b543d0371cd91dd39ceb7aeceb2895c228971
tehjawn <tehjawn@gmail.com>
2021-09-19 19:09:55 -0400

    Tutorial: Clean Up Hero Naming in Anonymous Functions

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -31,7 +31,7 @@ export class HeroDetailComponent implements OnInit {
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id)
-      .subscribe(hero => this.hero = hero);
+      .subscribe(h => this.hero = h);
   }
 
   save(): void {
