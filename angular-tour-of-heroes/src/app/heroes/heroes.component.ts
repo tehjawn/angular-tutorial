ee662694754e45cbd20ccf90e5ef288197aa2e74
tehjawn <tehjawn@gmail.com>
2021-09-19 14:35:25 -0400

    Tutorial: Implement Message Component for Logging

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,19 +10,28 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = [];
 
   selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
-  constructor(private heroService: HeroService) { }
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) { }
+
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(
         heroes => this.heroes = heroes
       );
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(
+      `HeroesComponent: Selected hero id=${hero.id}`
+    )
   }
 
   ngOnInit(): void {
