83d43bfac94de5231ce8b73f0d27b78a6b56037c
tehjawn <tehjawn@gmail.com>
2021-09-21 05:51:53 -0400

    Tutorial: Setup Dummy Hero Form Component

--- angular-hero-agency-employment-form/src/app/app.module.ts
+++ angular-hero-agency-employment-form/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroFormComponent
   ],
   imports: [
     BrowserModule
